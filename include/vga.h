b613722321f8ad8f1710d602a675bdb5bb82c20d
Florian Mehm <florian@mehm.net>
2023-12-27 17:53:50 +0100

    Merged changes

--- include/vga.h
+++ include/vga.h
@@ -1054,6 +1054,14 @@ struct VgaType {
 	bool ega_mode_with_vga_colors = false;
 };
 
+// Hercules & CGA monochrome palette
+enum class MonochromePalette : uint8_t {
+	Amber      = 0,
+	Green      = 1,
+	White      = 2,
+	Paperwhite = 3
+};
+
 // Hacky redraw during debug mode
 void VGA_Redraw(void);
 
