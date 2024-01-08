e3e11e768f40fe717541e077520bf75d1806f6ad
Florian Mehm <florian@mehm.net>
2024-01-08 16:43:15 +0100

    Worked on SIS

--- src/debug/debug.cpp
+++ src/debug/debug.cpp
@@ -3702,3 +3702,196 @@ void SIS_HandleGameLoad(Bitu seg, Bitu off) {
 }
 
 #include "debug_sis.cpp"
+
+// SIS Debug code below
+
+void SIS_Init()
+{
+	debugLogEnabled["special"]     = false;
+	debugLogEnabled["fileread"]    = false;
+	debugLogEnabled[SIS_AnimFrame] = false;
+}
+
+bool SIS_IsBreakpoint(Bitu seg, Bitu off)
+{
+	/* static bool hitOnce = false;
+	if (seg == 0x01E7 && off == 0xDB56 && !hitOnce) {
+	        hitOnce = true;
+	        return true;
+	} */
+	return false;
+}
+
+void SIS_Temp_HandleSkipDrawObject(Bitu seg, Bitu off)
+{
+	// Enabling one of the two blocks will skip either the first object or
+	// the second object
+	/* if (seg == 0x01E7 && off == 0x92E5) {
+	        // Skip handling the first object
+	        reg_ax = 0x2;
+	} */
+	/* if (seg == 0x01E7 && off == 0x987C) {
+	        reg_ax = 0x02;
+	} */
+}
+
+void SIS_LogAnimFrame(Bitu seg, Bitu off)
+{
+	if (!(seg == 0x01E7 && off == 0x95D2)) {
+		return;
+	}
+	uint16_t index = mem_readw_inline(GetAddress(SegValue(ss), reg_bp - 0x0A));
+	if (index != 2) {
+		return;
+	}
+
+	// Try to mess with what I think is the target width
+	// mem_writew_inline(GetAddress(SegValue(ss), reg_sp + 0x0A), 0x40);
+	// mem_writew_inline(GetAddress(SegValue(ss), reg_sp + 0x0C), 0x40);
+
+	// push	word ptr es:[di+2Eh] ;; [bp+1Ch]
+	uint16_t v1 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x16));
+	// push	word ptr es:[di+2Ch] ;; [bp+1Ah]
+	uint16_t v2 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x14));
+	// mov	ax,[bp-2h]
+	// shr	ax,1h
+	// mov	dx,ax
+	// les	di,[bp-1Ah]
+	// mov	ax,es:[di]
+	// sub	ax,dx
+	// push	ax ;; [bp+18h]
+	uint16_t v3 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x12));
+	// mov	ax,es:[di+2h]
+	// sub	ax,[bp-4h]
+	// sub	ax,[bp-8h]
+	// push	ax ;; [bp+16h]
+	uint16_t v4 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x10));
+	// push	2h ;; [bp+14h]
+	// TODO: Add the 2
+	uint16_t v5 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x0E));
+	// push	word ptr [bp-6h] ;; [bp+12h]
+	uint16_t v6 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x0C));
+	// push	word ptr es:[di+2h] ;; [bp+10h]
+	uint16_t v7 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x0A));
+
+	// les	di,[0778h]
+	// add	di,1013h
+	// push	es ;; [bp+Eh]
+	uint16_t v8 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x08));
+	// push	di ;; [bp+Ch]
+	uint16_t v9 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x06));
+	// les	di,[0778h]
+	// add	di,53D3h
+	// push	es ;; [bp+Ah]
+	uint16_t v10 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x04));
+	// push	di ;; [bp+8h]
+	uint16_t v11 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x02));
+	// push	word ptr [bp-14h] ;; [bp+6h]
+	uint16_t v12 = mem_readw_inline(GetAddress(SegValue(ss), reg_sp + 0x00));
+	// call	far 00B7h:172Ch
+
+	uint16_t bp12 = mem_readw_inline(GetAddress(SegValue(ss), reg_bp - 0x12));
+	fprintf(stdout,
+	        "Arguments for 172C call: bp-12h: %.4x, di: %.4x - %.4x %.4x %.4x %.4x %.4x %.4x %.4x %.4x %.4x %.4x %.4x %.4x \n",
+	        bp12,
+	        reg_di,
+	        v1,
+	        v2,
+	        v3,
+	        v4,
+	        v5,
+	        v6,
+	        v7,
+	        v8,
+	        v9,
+	        v10,
+	        v11,
+	        v12);
+}
+
+void SIS_HandleAnimFrame(Bitu seg, Bitu off)
+{
+	if (!debugLogEnabled[SIS_AnimFrame]) {
+		return;
+	}
+
+	if (seg != 0x01F7) {
+		return;
+	}
+
+	if (off == 0x1615) {
+		fprintf(stdout, "fn00B7_1480: Results of call: %.4x %.4x\n", reg_ax, reg_dx);
+	}
+}
+
+void SIS_HandleAnimFramePainting(Bitu seg, Bitu off)
+{
+	if (!debugLogEnabled[SIS_AnimFrame]) {
+		return;
+	}
+
+	if (seg != 0x01F7) {
+		return;
+	}
+
+	// l00B7_0FA4:
+	// mov	al,[si]
+
+	if (off == 0x0FC9) {
+		// mov es : [di], al
+		fprintf(stdout,
+		        "Writing pixel %.2x to di: %.4x (%.4x:%.4x)\n",
+		        reg_al,
+		        reg_di,
+		        SegValue(es),
+		        reg_di);
+	}
+}
+
+void SIS_HandleSIS(Bitu seg, Bitu off)
+{
+	// SIS_Temp_HandleSkipDrawObject(seg, off);
+	SIS_LogAnimFrame(seg, off);
+	SIS_HandleAnimFrame(seg, off);
+	SIS_HandleAnimFramePainting(seg, off);
+}
+
+bool SIS_ParseCommand(char* found, std::string command)
+{
+	if (command == "CALLER") {
+		bool all       = !(*found);
+		uint8_t levels = all ? 1 : (uint8_t)GetHexValue(found, found);
+
+		// At SS:BP, the old stack frame is saved, so we can use this to
+		// walk up
+		uint32_t calleeBP = reg_bp;
+		uint32_t callerBP = mem_readw_inline(
+		        GetAddress(SegValue(ss), calleeBP + 0x00));
+		uint32_t ret_off = mem_readw_inline(
+		        GetAddress(SegValue(ss), calleeBP + 0x02));
+		uint32_t ret_seg = mem_readw_inline(
+		        GetAddress(SegValue(ss), calleeBP + 0x04));
+
+		while (levels != 1) {
+			// If need be, walk up again the same way we did before,
+			// just that we read the caller's BP from the stack
+			calleeBP = callerBP;
+			callerBP = mem_readw_inline(
+			        GetAddress(SegValue(ss), calleeBP + 0x00));
+			ret_off = mem_readw_inline(
+			        GetAddress(SegValue(ss), calleeBP + 0x02));
+			ret_seg = mem_readw_inline(
+			        GetAddress(SegValue(ss), calleeBP + 0x04));
+			// TODO: Implement more than one level
+			levels--;
+		}
+
+		codeViewData.useCS     = ret_seg;
+		codeViewData.useEIP    = ret_off;
+		codeViewData.cursorPos = 0;
+
+		return true;
+	}
+
+	return false;
+}
